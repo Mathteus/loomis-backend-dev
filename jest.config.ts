98823047be733797235c61c721c3320f3bcec604
Mathteus <Mathteus18@outlook.com>
2025-08-19 13:56:42 -0300

    [MANUNTENÇÃO] Atualização nos arquivios de config

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,20 @@
-export default {
-  moduleFileExtensions: ['js', 'json', 'ts'],
-  rootDir: 'src',
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.json';
+import { Config } from 'jest';
+
+const config: Config = {
+  moduleFileExtensions: ['js', 'json', 'ts', 'tsx'],
   testRegex: '.*\\.spec\\.ts$',
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
   },
+
   collectCoverageFrom: ['**/*.(t|j)s'],
   coverageDirectory: '../coverage',
   testEnvironment: 'node',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>',
+  }),
 };
+
+export default config;
