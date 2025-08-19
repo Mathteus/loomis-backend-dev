b29a57cd31f7fc0f519b6a44e90ef1c0b18781a6
Mathteus <Mathteus18@outlook.com>
2025-08-19 14:10:25 -0300

    [FEATURE] criação de rotas e services para webhooks

---
+++ src/http/webhooks/webhooks.controller.ts
@@ -0,0 +1,26 @@
+import { Controller, Post, Req, Headers, Body } from '@nestjs/common';
+
+@Controller('webhooks')
+export class WebhooksController {
+  @Post('stripe')
+  async stripeWebhook(@Req() req, @Headers('stripe-signature') sig: string) {
+    // const event = this.gateways.parseStripeEvent(req.rawBody, sig);
+    // if (event.type === 'checkout.session.completed') {
+    // const { signupIntentId, email, customerId } = this.gateways.pickStripeD
+    //   await this.signup.completeFromGateway({ provider: 'STRIPE', signupInten
+    // }
+    // return { ok: true };
+  }
+
+  @Post('asaas')
+  async asaasWebhook(@Body() body: any) {
+    // const { signupIntentId, email, customerId } = await this.gateways.pickAsa
+    // await this.signup.completeFromGateway({ provider: 'ASAAS', signupIntentId
+    // return { ok: true };
+  }
+
+  @Post('')
+  async selfWebhook() {
+    
+  }
+}
