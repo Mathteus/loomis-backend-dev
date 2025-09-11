69a3376838c29dd42e42bf11aafcb1d2756f539f
Mathteus <Mathteus18@outlook.com>
2025-09-11 20:56:19 -0300

    [FEATURE] classe para hash customizada

--- src/common/hash/hash-generator.service.ts
+++ src/common/hash/hash-generator.service.ts
@@ -1,7 +1,9 @@
 import { randomString } from '@/utility';
+import { Injectable } from '@nestjs/common';
 import { createHash, randomUUID } from 'node:crypto';
 
-export class HashGenerator {
+@Injectable()
+export class HashGeneratorService {
   createSHA256(salt: string = randomString(10)) {
     const now = new Date().toISOString();
     return createHash('sha256').update(`${now}:${salt}`).digest('hex');
